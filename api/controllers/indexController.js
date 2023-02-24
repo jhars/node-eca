1999fedd222526cf3802f92262d8d87a8c6a51dd
jhars <jonharlan.io@gmail.com>
2023-02-24 10:14:40 -0500

    add index  / route

---
+++ api/controllers/indexController.js
@@ -0,0 +1,5 @@
+const index = async (req, res) => {
+    res.json("Index of Express App, JH")
+}
+
+exports.index = index
--- api/routes.js
+++ api/routes.js
@@ -1,10 +1,14 @@
 'use strict';
+const index = require('./controllers/indexController')
 const sftp = require('./controllers/sftpController')
 const goods = require('./controllers/goodsByKitNumberController')
 // const user =  require('./controllers/userController')
 const liveDeliverySchedule = require('./controllers/liveDeliveryScheduleController')
 
 module.exports = function (app) {
+    app.route('/')
+        .get(index.index)
+
     app.route('/sftp/list')
         .get(sftp.listFiles)
 
